683661d839655a5862980f1302dea3de53f006af
Mahanotrahul <thecodeboxed@gmail.com>
2020-03-07 23:56:03 +0530

    Updated setup.py file

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
+def _get_requirements():
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
+    return requirements
+
 setup(
       name = 'sockets',
       version = '1.0',
       description = 'Testing installation of sockets package',
-      packages = ['sockets'],
+      packages = ['mysocks'],
+      license='MIT',
+      url='https://github.com/Mahanotrahul/mysocks',
       author = 'Rahul Mahanot',
-      license = 'MIT',
+      install_requires=_get_requirements(),
       author_email = 'thecodeboxed@gmail.com')
