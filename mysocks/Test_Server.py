750e9b1ce12146ea29ad6fddfe2e0e3bce5979b4
Mahanotrahul <thecodeboxed@gmail.com>
2020-03-08 21:13:11 +0530

    Indentation Bug Fix

--- mysocks/Test_Server.py
+++ mysocks/Test_Server.py
@@ -31,31 +31,31 @@ class sockets:
             sys.exit()
 
     def accept_connections(self):
-            while True:
-        		conn, addr = s.accept()
-        		print('New client connected. Address = ' + str(addr[0]) + ' ' + str(addr[1]))
-        		u_name = conn.recv(1024)
-        		u_name = u_name.decode('utf-8')
-        		u_name = u_name.replace('USER ', '')
-        		print('user id = ' + str(u_name))
-        		g_name = conn.recv(1024)
-        		g_name = g_name.decode('utf-8')
-        		g_name = g_name.replace('JOIN ', '')
-        		print('group_id = ' + str(g_name))
-        		all_connections.append(conn)
-        		all_names.append(u_name)
+        while True:
+        	conn, addr = s.accept()
+        	print('New client connected. Address = ' + str(addr[0]) + ' ' + str(addr[1]))
+        	u_name = conn.recv(1024)
+        	u_name = u_name.decode('utf-8')
+        	u_name = u_name.replace('USER ', '')
+        	print('user id = ' + str(u_name))
+        	g_name = conn.recv(1024)
+        	g_name = g_name.decode('utf-8')
+        	g_name = g_name.replace('JOIN ', '')
+        	print('group_id = ' + str(g_name))
+        	all_connections.append(conn)
+        	all_names.append(u_name)
 
 
-        		if(g_name in group_names):
-        			t = threading.Thread(target = receive_data, args = (g_name, conn, u_name))
-        			t.daemon = True
-        			t.start()
-        		else:
-        			group_names.append(g_name)
-        			t = threading.Thread(target = receive_data, args = (g_name, conn, u_name))
-        			t.daemon = True
-        			t.start()
-                    
+        	if(g_name in group_names):
+        		t = threading.Thread(target = receive_data, args = (g_name, conn, u_name))
+        		t.daemon = True
+        		t.start()
+        	else:
+        		group_names.append(g_name)
+        		t = threading.Thread(target = receive_data, args = (g_name, conn, u_name))
+        		t.daemon = True
+        		t.start()
+
     def file_transfer(self, src, dest):
         pass
 
