c488687b6bb1a7bb48c950e873110a985203aad7
Mahanotrahul <thecodeboxed@gmail.com>
2020-03-08 17:39:38 +0530

    Feature to transfer multiple files

--- mysocks/file_transfer.py
+++ mysocks/file_transfer.py
@@ -6,6 +6,7 @@ import errno
 from tqdm import tqdm
 from mysocks.utilities import mapcount
 import time
+from math import floor, ceil
 
 from . import Model
 
@@ -34,19 +35,19 @@ class send_file(Model):
     Call this class to send file_send
     """
 
-    def __init__(self, filename, host = '127.0.0.1', port = 5560, n_listen = 1):
+    def __init__(self, filenames, host = '127.0.0.1', port = 5560, n_listen = 1):
         """
         Constructor to start the file transfer process
         """
-        if filename == None:
-            print('Filename cannot be none')
-        elif not os.path.exists(filename):
-            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filename)
+        if len(filenames) == 0:
+            print('Filename should be passed as a list of all file locations. Empty list provided')
+        # elif not all(os.path.exists(filename) for filename in filenames):
+        #     raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filenames)
         # super(Model, self).__init__()
 
         self.s = super().create_server_socket(host, port, n_listen)
 
-        self.send_file(filename)
+        self.send_file(filenames)
 
     def accept_connections(self):
         print("Waiting for client to connect")
@@ -58,7 +59,7 @@ class send_file(Model):
         """Returns the current line number in our program."""
         return inspect.currentframe().f_back.f_lineno
 
-    def send_file(self, filename):
+    def send_file(self, filenames):
 
         # try:
             if 'conn' not in locals():
@@ -69,22 +70,44 @@ class send_file(Model):
                 confirm = conn.recv(1024)   ## Receive confirmation from the client. Expected value - '1'
                 if confirm.decode('utf-8') == '1':
                     print("Confirmation received.")
-                    print("Sending File : " + ntpath.basename(filename))
-                    conn.send(str.encode(ntpath.basename(filename)))
-                    time.sleep(0.05)
-                    file_size = os.path.getsize(filename)
 
-                    conn.send(str.encode(str(file_size)))       ## sends file size as Metadata
-                    # conn.send(str.encode('---- File starts ----\n'))
-                    f =  open(filename, 'rb')
-                    l = f.read(1024)
-                    pbar = tqdm(total = int(file_size)//1024)
-                    while(l):
-                        conn.send(l)
+                    # Sending Total number of files being sent to the client
+                    no_files = len(filenames)
+
+                    # TODO: Put valid file check in a separate utility Function
+                    for filename in filenames:
+                        if not os.path.exists(filename):
+                            print('Error : ' + str(filename) + ' does not exist. Check the file location. Skipping transfer of this file')
+                            no_files -= 1
+                            continue
+
+                    conn.send(str.encode(str(no_files)))
+                    pbar1 = tqdm(total = int(no_files), initial = 0)
+
+                    for filename in filenames:
+                        if not os.path.exists(filename):
+                            # print('Error : ' + str(filename) + ' does not exist. Check the file location. Skipping transfer of this file')
+                            continue
+                        print("Sending File : " + ntpath.basename(filename))
+                        conn.send(str.encode(ntpath.basename(filename)))
+                        time.sleep(0.05)
+                        file_size = os.path.getsize(filename)
+
+                        conn.send(str.encode(str(file_size)))       ## sends file size as Metadata
+                        # conn.send(str.encode('---- File starts ----\n'))
+                        f =  open(filename, 'rb')
                         l = f.read(1024)
-                        pbar.update(1)
-                    pbar.close()
-                    f.close()
+                        pbar = tqdm(total = int(file_size)//1024)
+                        while(l):
+                            conn.send(l)
+                            l = f.read(1024)
+                            pbar.update(1)
+                        pbar.close()
+                        f.close()
+                        conn.send(str.encode(''))
+                        pbar1.update(1)
+
+                    pbar1.close()
                     print('Done sending')
                     conn.send(str.encode(''))
                     print("Closing Connection")
@@ -117,23 +140,43 @@ class receive_file(Model):
         Method to receive files from the socket server
         """
         self.s.send(str.encode('1'))
-        file_name = self.s.recv(1024).decode('utf-8')
-        print(file_name)
-        file_ext = file_name.split('.')[-1]
-        file_size = self.s.recv(1024).decode('utf-8')
+        no_files = int(self.s.recv(1024))
+        pbar1 = tqdm(total = no_files, initial = 0)
+        no_files_received = 0
 
-        with open(os.path.join(self.save_path, str(file_name)), 'wb') as f:
-            print('Receiving File')
-            print(int(file_size))
-            pbar = tqdm(total = int(file_size)//1024)
-            while True:
-                data = self.s.recv(1024)
-                pbar.update(1)
-                if not data:
-                    break;
-                f.write(data)
-            pbar.close()
-            f.close()
-        print('Successfully received file.')
+        for i in range(no_files):
+            file_name = self.s.recv(1024)
+            print(file_name)
+            file_ext = file_name.split('.')[-1]
+            file_size = float(self.s.recv(1024))
+
+            with open(os.path.join(self.save_path, str(file_name)), 'wb') as f:
+                print('Receiving File')
+                print(floor(file_size/1024))
+                pbar = tqdm(total = int(file_size)//1024)
+                if floor(file_size/1024) - ceil(file_size/1024) == 0:
+                    for _ in range(floor(file_size/1024)):
+                        print("In the integral multiple section")
+                        data = self.s.recv(1024)
+                        pbar.update(1)
+                        f.write(data)
+                else:
+                    print("NOT In the integral multiple section")
+                    for _ in range(floor(file_size/1024)):
+                        data = self.s.recv(1024)
+                        pbar.update(1)
+                        f.write(data)
+                    print("NOT In the integral multiple section")
+                    # Receiving one more time as the file size is not a multiple of 1024
+                    # Hence, One last chunk of data is still yet to be received
+                    data = self.s.recv(1024)
+                    pbar.update(1)
+                    f.write(data)
+                pbar.close()
+                f.close()
+            no_files_received += 1
+            pbar1.update(1)
+        pbar1.close()
+        print('Successfully received ' + str(no_files_received) + ' files.')
         self.s.close()
         print('connection closed')
